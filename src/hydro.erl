2c77d6d182063c735aed9bb9c4ddae913c35c85b
Bryan Paxton <starbelly@pobox.com>
2018-12-30 23:18:24 -0600

    add some types

--- src/hydro.erl
+++ src/hydro.erl
@@ -2,6 +2,9 @@
 %%% @end.
 -module(hydro).
 
+-include("hydro.hrl").
+
+
 -define(KEYGENS,
         #{
           hash => hash_keygen,
@@ -28,8 +31,8 @@
 bin2hex(Bin) ->
     hydro_api:bin2hex(Bin).
 
--spec box_seal(binary(), binary()) -> 
-    {ok, binary(), binary()} | {error, term()}.
+-spec box_seal(ctx(), key()) -> 
+    {ok, hash(), key()} | {error, term()}.
 box_seal(C, M) -> 
     K = hydro_api:secretbox_keygen(),
     case hydro_api:secretbox_encrypt(C, M, K) of 
@@ -37,13 +40,13 @@ box_seal(C, M) ->
         {error, _} = Err -> Err
     end.
 
--spec box_open(binary(), binary(), binary()) -> 
+-spec box_open(ctx(), hash(), key()) -> 
     {ok, binary()} | {error, term()}.
 box_open(C, H, K) -> 
     hydro_api:secretbox_decrypt(C, H, K).
 
--spec box_seal(binary(), binary(), integer()) -> 
-    {ok, binary(), binary()} | {error, term()}.
+-spec box_seal(ctx(), msg(), id()) -> 
+    {ok, hash(), key()} | {error, term()}.
 box_seal(C, M, I) -> 
     K = hydro_api:secretbox_keygen(),
     case hydro_api:secretbox_encrypt(C, M, I, K) of 
@@ -51,8 +54,8 @@ box_seal(C, M, I) ->
         {error, _} = Err -> Err
     end.
 
--spec box_open(binary(), binary(), integer(), binary()) -> 
-    {ok, binary()} | {error, term()}.
+-spec box_open(ctx(), hash(), id(), key()) -> 
+    {ok, msg()} | {error, term()}.
 box_open(C, H, I, K) -> 
     hydro_api:secretbox_decrypt(C, H, I, K).
 
@@ -60,25 +63,25 @@ box_open(C, H, I, K) ->
 rand(N) when N >= 0 ->
     hydro_api:random_buf(N).
 
--spec rand_uniform(non_neg_integer()) -> non_neg_integer().
+-spec rand_uniform(buf_size()) -> non_neg_integer().
 rand_uniform(N) when N >= 0 ->
     hydro_api:random_uniform(N).
 
 %%% @doc
 %%% Generates a key suitable for working with the generic hashing functions
 %%% @end
--spec hash_keygen() -> binary().
+-spec hash_keygen() -> key().
 hash_keygen() -> 
     hydro_api:hash_keygen().
 
--spec keygen(atom()) -> binary().
+-spec keygen(key_type()) -> key().
 keygen(KeyType) when is_atom(KeyType) ->
     case maps:get(KeyType, ?KEYGENS, none) of
         none -> {error, unknown_key_type};
         FunName -> hydro_api:FunName()
     end.
 
--spec keygen_pair(atom()) -> {ok, binary(), binary()}.
+-spec keygen_pair(keypair_type()) -> {ok, key(), key()}.
 keygen_pair(KeyPairType) when is_atom(KeyPairType) ->
     case maps:get(KeyPairType, ?KEYGEN_PAIRS, none) of
         none -> {error, unknown_key_type};
@@ -89,7 +92,7 @@ keygen_pair(KeyPairType) when is_atom(KeyPairType) ->
 %% The hash4 function returns a computed fixed-length finger print (hash)
 %% using the supplied context, message, key, and size. Size must be between 32 and 65535.
 %% @end
--spec hash(binary(), binary()) -> {ok, binary()} | {error,term()}.
+-spec hash(ctx(), msg()) -> {ok, hash()} | {error,term()}.
 hash(Context, Msg) when is_binary(Msg)
                          andalso  is_binary(Msg) ->
     hydro_api:hash_hash(Msg, Context).
@@ -98,7 +101,7 @@ hash(Context, Msg) when is_binary(Msg)
 %% The hash4 function returns a computed fixed-length finger print (hash)
 %% using the supplied context, message, key, and size. Size must be between 32 and 65535.
 %% @end
--spec hash(binary(), binary(), binary()) -> {ok, binary()} | {error,term()}.
+-spec hash(ctx(), msg(), key()) -> {ok, hash()} | {error,term()}.
 hash(Context, Msg, Key) when is_binary(Msg)
                               andalso is_binary(Msg)
                               andalso  is_binary(Key) ->
@@ -108,7 +111,7 @@ hash(Context, Msg, Key) when is_binary(Msg)
 %% The hash_init/2 initializes state of `Context' context with no key for a multi-part hash
 %% operation. Updates to the state may be perfomed using returned reference and hash_update/2
 %% @end
--spec hash_init(binary()) -> {ok, reference()} | {error, term()}.
+-spec hash_init(ctx()) -> {ok, hash_state()} | {error, term()}.
 hash_init(Context) when is_binary(Context) ->
     hydro_api:hash_init(Context).
 
@@ -116,7 +119,7 @@ hash_init(Context) when is_binary(Context) ->
 %% The hash_init/2 initializes state with a context of `Context', size `Size', and key `Key' for a multi-part hash
 %% operation. Updates to the state may be perfomed using returned reference and hash_update/2
 %% @end
--spec hash_init(binary(), binary()) -> {ok, reference()} | {error, term()}.
+-spec hash_init(ctx(), key()) -> {ok, hash_state()} | {error, term()}.
 hash_init(Context, Key) when is_binary(Context) 
                         andalso is_binary(Key) ->
     hydro_api:hash_init(Context, Key).
@@ -124,7 +127,7 @@ hash_init(Context, Key) when is_binary(Context)
 %% @doc
 %% The hash_update/2 updates the referenced state with the supplied message.
 %% @end
--spec hash_update(reference(), binary()) -> {ok, reference()} | {error, term()}.
+-spec hash_update(hash_state(), msg()) -> {ok, hash_state()} | {error, term()}.
 hash_update(State, Msg) when is_reference(State)
                         andalso is_binary(Msg) ->
     hydro_api:hash_update(State, Msg).
@@ -133,10 +136,10 @@ hash_update(State, Msg) when is_reference(State)
 %% The hash_final/2 functions returns a complete hash given a reference to a
 %% hash state and an output size.
 %% @end
--spec hash_final(reference()) -> {ok, binary()} | {error, term()}.
+-spec hash_final(hash_state()) -> {ok, hash()} | {error, term()}.
 hash_final(State) when is_reference(State) -> 
     hydro_api:hash_final(State).
 
--spec dice() -> integer().
+-spec dice() -> non_neg_integer().
 dice() -> 
     hydro_api:random_u32().
--- src/hydro_api.erl
+++ src/hydro_api.erl
@@ -5,6 +5,8 @@
 -define(APPNAME, hydro).
 -define(LIBNAME, hydro_nif).
 
+-include("hydro.hrl").
+
 -on_load(init/0).
 
 % helpers
@@ -68,11 +70,11 @@ bin2hex(Bin) ->
 hash_keygen() -> 
     hydro_hash_keygen().
 
--spec hash_hash(binary(), binary()) -> {ok, binary()} | {error, term()}.
+-spec hash_hash(msg(), ctx()) -> {ok, binary()} | {error, term()}.
 hash_hash(Msg, Context) -> 
     hydro_hash_hash(Msg, Context, <<"">>).
 
--spec hash_hash(binary(), binary(), binary()) -> {ok, binary()} | {error, term()}.
+-spec hash_hash(msg(), ctx(), key()) -> {ok, hash()} | {error, term()}.
 hash_hash(Msg, Context, Key) -> 
     hydro_hash_hash(Msg, Context, Key).
 
@@ -84,11 +86,11 @@ hash_init(Context) ->
 hash_init(Context, Key) -> 
     hydro_hash_init(Context, Key).
 
--spec hash_update(reference(), binary()) -> {ok, reference()} | {error, term()}.
+-spec hash_update(hash_state(), msg()) -> {ok, hash_state()} | {error, term()}.
 hash_update(State, Msg) -> 
     hydro_hash_update(State, Msg).
 
--spec hash_final(reference()) -> {ok, binary()} | {error, term()}.
+-spec hash_final(hash_state()) -> {ok, hash()} | {error, term()}.
 hash_final(State) -> 
     hydro_hash_final(State).
 
@@ -100,61 +102,92 @@ kdf_keygen() ->
 pwhash_keygen() -> 
     hydro_pwhash_keygen().
 
--spec pwhash_deterministic(_,_,_,_,_) -> any().
+-spec pwhash_deterministic(ctx(),passwd(),master_key(),integer(),ops_limit()) -> any().
 pwhash_deterministic(C, P, Mk, S, OpsLimit) -> 
     hydro_pwhash_deterministic(C, P, Mk, S, OpsLimit).
 
--spec pwhash_create(_,_,_,_,_) -> any().
+-spec pwhash_create(
+          passwd()
+        , master_key()
+        , ops_limit()
+        , mem_limit()
+        , thread_limit()) -> {ok, hash()}.
 pwhash_create(P, Mk, Ol, Ml, Tl) -> 
     hydro_pwhash_create(P, Mk, Ol, Ml, Tl).
 
--spec pwhash_verify(_,_,_,_,_,_) -> any().
+-spec pwhash_verify(
+          hash()
+        , passwd()
+        , master_key()
+        , ops_limit()
+        , mem_limit()
+        , thread_limit()) -> boolean().
 pwhash_verify(H, P, Mk, Ol, Ml, Tl) -> 
     hydro_pwhash_verify(H, P, Mk, Ol, Ml, Tl).
 
--spec pwhash_derive_static_key(_,_,_,_,_,_,_) -> any().
+-spec pwhash_derive_static_key(
+          ctx()
+        , hash()
+        , passwd()
+        , master_key()
+        , ops_limit()
+        , mem_limit()
+        , thread_limit()) -> {ok, key()}.
 pwhash_derive_static_key(C, H, P, Mk, Ol, Ml, Tl) -> 
     hydro_pwhash_derive_static_key(C, H, P, Mk, Ol, Ml, Tl).
 
--spec pwhash_reencrypt(_,_,_) -> any().
+-spec pwhash_reencrypt(hash(),master_key(),master_key()) -> any().
 pwhash_reencrypt(H, M, N) ->
     hydro_pwhash_reencrypt(H, M, N).
 
--spec pwhash_upgrade(_,_,_,_,_) -> any().
+-spec pwhash_upgrade(
+          hash()
+        , msg()
+        , ops_limit()
+        , mem_limit()
+        , thread_limit()) -> {ok, hash()}.
 pwhash_upgrade(H, M, Ol, Ml, Tl) ->
     hydro_pwhash_upgrade(H, M, Ol, Ml, Tl).
 
--spec secretbox_keygen() -> binary().
+-spec secretbox_keygen() -> key().
 secretbox_keygen() -> 
     hydro_secretbox_keygen().
 
--spec secretbox_encrypt(binary(), binary(), binary()) -> {ok, binary()} |
+-spec secretbox_encrypt(ctx(), msg(), key()) -> {ok, hash()} |
                                                          {error, term()}.
 secretbox_encrypt(C, M, K) -> 
     hydro_secretbox_encrypt(C, M, 0, K).
 
--spec secretbox_decrypt(binary(), binary(), binary()) -> {ok, binary()} |
+-spec secretbox_decrypt(ctx(), hash(), key()) -> {ok, msg()} |
                                                          {error, term()}.
 secretbox_decrypt(C, H, K) -> 
     hydro_secretbox_decrypt(C, H, 0, K).
 
--spec secretbox_encrypt(binary(), binary(), integer(), binary()) -> 
-    {ok, binary()} | {error, term()}.
+-spec secretbox_encrypt(ctx(), msg(), integer(), key()) -> 
+    {ok, hash()} | {error, term()}.
 secretbox_encrypt(C, M, I,  K) -> 
     hydro_secretbox_encrypt(C, M, I, K).
 
--spec secretbox_decrypt(binary(), binary(), integer(), binary()) -> {ok, binary()} |
-                                                         {error, term()}.
+-spec secretbox_decrypt(
+          ctx()
+        , hash()
+        , integer()
+        , key()) -> {ok, msg()} | {error, term()}.
 secretbox_decrypt(C, H, I, K) -> 
     hydro_secretbox_decrypt(C, H, I, K).
 
--spec secretbox_probe_create(binary(), binary(), binary()) -> {ok, binary()} |
-                                                         {error, term()}.
+-spec secretbox_probe_create(
+          ctx()
+        , hash() 
+        , key()) -> {ok, secretbox_probe()} | {error, term()}.
 secretbox_probe_create(C, H, K) -> 
     hydro_secretbox_probe_create(C, H, K).
 
--spec secretbox_probe_verify(binary(), binary(), binary(), binary()) -> {ok, binary()} |
-                                                         {error, term()}.
+-spec secretbox_probe_verify(
+          ctx()
+        , msg()
+        , key()
+        , secretbox_probe()) -> {ok, binary()} | {error, term()}.
 secretbox_probe_verify(C, M, K, P) -> 
     hydro_secretbox_probe_verify(C, M, K, P).
 
@@ -162,44 +195,47 @@ secretbox_probe_verify(C, M, K, P) ->
 sign_keygen() -> 
     hydro_sign_keygen().
 
--spec sign_create(binary(), binary(), binary()) -> {ok, binary()}.
+-spec sign_create(ctx(), msg(), sign_key()) -> {ok, pub_key()}.
 sign_create(C, M, K) -> 
     hydro_sign_create(C, M, K).
 
--spec sign_verify(binary(), binary(), binary(), binary()) -> boolean().
+-spec sign_verify(ctx(), msg(), sign_key(), pub_key()) -> boolean().
 sign_verify(C, M, S, K) -> 
     hydro_sign_verify(C, M, S, K).
 
--spec sign_init(binary()) -> {ok, reference()}.
+-spec sign_init(ctx()) -> {ok, sign_state()}.
 sign_init(C) -> 
     hydro_sign_init(C).
 
--spec sign_update(reference(), binary()) -> {ok, reference()}.
+-spec sign_update(sign_state(), msg()) -> {ok, sign_state()}.
 sign_update(State, M) -> 
     hydro_sign_update(State, M).
 
--spec sign_final_create(reference(), binary()) -> {ok, binary()}.
+-spec sign_final_create(sign_state(), sign_key()) -> {ok, pub_sig()}.
 sign_final_create(State, Sk) -> 
     hydro_sign_final_create(State, Sk).
 
--spec sign_final_verify(reference(), binary(), binary()) -> {ok, binary()}.
+-spec sign_final_verify(sign_state(), pub_sig(), pub_key()) -> {ok, binary()}.
 sign_final_verify(State, Sig, Pk) -> 
     hydro_sign_final_verify(State, Sig, Pk).
 
--spec kdf_derive_from_key(binary(), binary(), integer(), integer()) -> 
-    {ok, binary()} | {error, term()}.
+-spec kdf_derive_from_key(
+          ctx()
+        , master_key()
+        , id()
+        , integer()) -> {ok, binary()} | {error, term()}.
 kdf_derive_from_key(Ctx, Master, SubId, Size) ->
     hydro_kdf_derive_from_key(Ctx, Master, SubId, Size).
 
--spec random_buf(non_neg_integer()) -> binary().
+-spec random_buf(buf_size()) -> binary().
 random_buf(N) when N >= 0 ->
     hydro_random_buf(N).
 
--spec random_buf_deterministic(non_neg_integer(), binary()) -> binary().
+-spec random_buf_deterministic(buf_size(), rnd_seed()) -> binary().
 random_buf_deterministic(Size, Seed) when Size >= 0 ->
     hydro_random_buf_deterministic(Size, Seed).
 
--spec random_uniform(non_neg_integer()) -> integer().
+-spec random_uniform(buf_size()) -> non_neg_integer().
 random_uniform(N) when N >= 0 ->
     hydro_random_uniform(N).
 
