fc16a3f2f6e9b9ba1c32552363ade85b0e4ea038
Bryan Paxton <starbelly@pobox.com>
2019-01-27 05:32:56 -0600

    Generate tests for generic hashing in parallel

--- test/hydro_api_tests.erl
+++ test/hydro_api_tests.erl
@@ -4,14 +4,36 @@
 
 %% The expected results below were obtained directly from libhydrogen in C
 
-generichash_keyless_test() -> 
+api_test_() ->
+   api_harness_gen(1000).
+
+api_harness_gen(N) ->
+    List =  {setup, fun setup/0, fun cleanup/1,
+             {inparallel, [ fun generichash/0,
+                            fun generichash_multi/0,
+                            fun generichash_keyless/0,
+                            fun generichash_multi_keyless/0]}},
+    {generator,
+     fun () ->
+             if N > 0 ->
+                    [ List | api_harness_gen(N-1)];
+                true ->
+                    []
+             end
+     end}.
+
+setup() -> ok.
+
+cleanup(_Eh) -> ok.
+
+generichash_keyless() ->
     Expected =
     <<"5112339920a88d57c19c9ed8ab44e0a07b5ece2a7bba461d7878b4d7ee3df43c">>,
     {ok, H} = hydro_api:hash_hash(<<"There is plenty for the both of us, may the best Dwarf win.">>, <<"libtests">>),
     Hex = hydro_api:bin2hex(H),
     ?assertEqual(Hex, Expected).
 
-generichash_multi_keyless_test() -> 
+generichash_multi_keyless() ->
     Str1 = <<"There is plenty for the both of us, ">>,
     Str2 = <<"may the best Dwarf win.">>,
     Expected =
@@ -23,7 +45,7 @@ generichash_multi_keyless_test() ->
     Hex = hydro_api:bin2hex(H),
     ?assertEqual(Hex, Expected).
 
-generichash_test() -> 
+generichash() ->
     Expected =
     <<"87309f1d3f269b023f25c7255aaed441e37fb81c0404b31a43061cae64d59874">>,
     Key =  <<"Nobody tosses a dwarf and foobar">>,
@@ -32,7 +54,7 @@ generichash_test() ->
     Hex = hydro_api:bin2hex(H),
     ?assertEqual(Hex, Expected).
 
-generichash_multi_test() -> 
+generichash_multi() ->
     Str1 = <<"There is plenty for the both of us, ">>,
     Str2 = <<"may the best Dwarf win.">>,
     Key = <<"Nobody tosses a dwarf and foobar">>,
